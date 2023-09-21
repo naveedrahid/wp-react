f84192b32bddfb1df0aafbedebd32cf2fe8b503e
naveedrahid <naveedrashid685@gmail.com>
2023-09-21 19:24:27 +0500

    new section scroll counter

--- src/Components/Pages/home/Home.js
+++ src/Components/Pages/home/Home.js
@@ -1,54 +1,97 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Col, Container, Row } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBolt } from '@fortawesome/free-solid-svg-icons';
 import './Home.css'
 
 function Home() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const secondSection = document.getElementsByClassName('greatExperience')[0]; // Access the first element
+    if (secondSection) {
+      const rect = secondSection.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom >= 0) {
+        incrementCounter();
+      }
+    }
+  };
+
+
   return (
     <div className='mainContent'>
-        <section className='bannerSlider d-flex align-items-center'>
-          <Container>
-            <Row className='align-items-center'>
-              <Col xs={12} md={{span:8, offset:2}}>
-                <h1 className='text-center mb-5'>All in One Personal Portfolio for <strong>Developer</strong></h1>
-              </Col>
-              <Col xs={12} sm={12} md={4}>
-                <div className='devServices'>
-                  <div className='devServicesIcon'>
-                    <FontAwesomeIcon icon={faBolt} />
-                  </div>
-                  <div className='devServicesInfo'>
-                    <h2>Fast Performance</h2>
-                    <p>Optimized for a smaller build size, faster dev compilation and dozens of other improvements.</p>
-                  </div>
+      <section className='bannerSlider d-flex align-items-center'>
+        <Container>
+          <Row className='align-items-center'>
+            <Col xs={12} md={6}>
+              <h1>Howdy, I'm Ramsay</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} sm={12} md={4}>
+              <div className='devServices'>
+                <div className='devServicesIcon'>
+                  <FontAwesomeIcon icon={faBolt} />
                 </div>
-              </Col>
-              <Col xs={12} sm={12} md={4}>
-                <div className='devServices'>
-                  <div className='devServicesIcon'>
-                    <FontAwesomeIcon icon={faBolt} />
-                  </div>
-                  <div className='devServicesInfo'>
-                    <h2>Fast Performance</h2>
-                    <p>Optimized for a smaller build size, faster dev compilation and dozens of other improvements.</p>
-                  </div>
+                <div className='devServicesInfo'>
+                  <h2>Fast Performance</h2>
+                  <p>Optimized for a smaller build size, faster dev compilation and dozens of other improvements.</p>
                 </div>
-              </Col>
-              <Col xs={12} sm={12} md={4}>
-                <div className='devServices'>
-                  <div className='devServicesIcon'>
-                    <FontAwesomeIcon icon={faBolt} />
-                  </div>
-                  <div className='devServicesInfo'>
-                    <h2>Fast Performance</h2>
-                    <p>Optimized for a smaller build size, faster dev compilation and dozens of other improvements.</p>
-                  </div>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={4}>
+              <div className='devServices'>
+                <div className='devServicesIcon'>
+                  <FontAwesomeIcon icon={faBolt} />
                 </div>
-              </Col>
-            </Row>
-          </Container>
-        </section>
+                <div className='devServicesInfo'>
+                  <h2>Fast Performance</h2>
+                  <p>Optimized for a smaller build size, faster dev compilation and dozens of other improvements.</p>
+                </div>
+              </div>
+            </Col>
+            <Col xs={12} sm={12} md={4}>
+              <div className='devServices'>
+                <div className='devServicesIcon'>
+                  <FontAwesomeIcon icon={faBolt} />
+                </div>
+                <div className='devServicesInfo'>
+                  <h2>Fast Performance</h2>
+                  <p>Optimized for a smaller build size, faster dev compilation and dozens of other improvements.</p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className='greatExperience'>
+        <Container>
+        <Col xs={12} sm={6} md={6}>
+          <h2>Counter: {count}</h2>
+          <h2>sdadasdas</h2>
+          <h2>sdadasdas</h2>
+          <h2>sdadasdas</h2>
+          <h2>sdadasdas</h2>
+          <h2>sdadasdas</h2>
+          <h2>sdadasdas</h2>
+          <h2>sdadasdas</h2>
+        </Col>
+        <Col xs={12} sm={6} md={6}>
+
+        </Col>
+        </Container>
+      </section>
     </div>
   )
 }
--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import './Header.css'
 
 function Header() {
   return (
-    <Navbar className='mainHeader'>
-      <Container fluid>
-        <Navbar.Brand><Link to={'/'}><img src={process.env.PUBLIC_URL + '/assets/images/you-logo-here.png'} width={'30%'} alt=''/></Link></Navbar.Brand>
+    <Navbar className="mainHeader">
+      <Container >
+        <Navbar.Brand><Link to={'/'}><img src={process.env.PUBLIC_URL + '/assets/images/you-logo-here.png'} width={'30%'} alt='' /></Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ms-auto menuItems'>
-            <Nav.Item>
-              <Link to={'/'}>Home</Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link to={'/contact'}>Contact</Link>
-            </Nav.Item>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav><Link to={'/'}>Home</Link></Nav>
+            <Nav><Link to={'/contact'}>Contact</Link></Nav>
             <Nav.Item className='btnPrim'>
               <Button variant="dark">Contact Now</Button>
             </Nav.Item>
